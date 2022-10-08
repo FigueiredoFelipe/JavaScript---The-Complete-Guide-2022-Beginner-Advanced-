efe6ddb64c116fd7939c58ab30fc7dec81648084
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2022-10-08 18:32:07 -0300

    update 10-8-2022

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
 function add(num1, num2) {
-  currentResult = currentResult + +userInput.value;
-  outputResult(currentResult, `${currentResult} + ${}`);
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
